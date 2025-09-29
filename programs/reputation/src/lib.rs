a76af761ab4bfa37b6195273ab27d24630fa8cf4
Khushal <khushal2004a@gmail.com>
2025-09-29 09:12:43 +0530

    feat: score_attestor and reputation program

---
+++ programs/reputation/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("6uLcVgj2viBHA5niMsXvKHuxMVvTRFjvgRrD933L8pk3");
+
+#[program]
+pub mod reputation {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
