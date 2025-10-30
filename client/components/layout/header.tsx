0434697ecf1a2169b3337e5b88e8bd779a22031f
Khushal <khushal2004a@gmail.com>
2025-10-30 18:47:28 +0530

    fix: conflict

--- client/components/layout/header.tsx
+++ client/components/layout/header.tsx
@@ -48,11 +48,7 @@ export function Header() {
                     height={60}
                     />
                     <span className="hidden sm:inline text-lg font-semibold bg-gradient-to-r from-violet-600 to-blue-600 dark:from-violet-400 dark:to-blue-400 bg-clip-text text-transparent">
-<<<<<<< Updated upstream
-                        CrediSol
-=======
                         CrediSOL
->>>>>>> Stashed changes
                     </span>
                 </motion.a>
 
